620ebd79f3c9f88015935db734e1a7e905674263
rfacun <rossi_facundo@outlook.com>
2023-07-20 16:22:20 -0300

    modified default theme so env var is no longer needed

--- config.h
+++ config.h
@@ -5,16 +5,17 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	//"monospace:size=10"
+	"Noto Sans Mono:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#b3b1ad", "#0a0e14" },
+	[SchemeSel] = { "#0a0e14", "#e09d47" },
+	[SchemeSelHighlight] = { "#0a0e14", "#b37d38" },
+	[SchemeNormHighlight] = { "#e09d47", "#0a0e14" },
+	[SchemeOut] = { "#0a0e14", "#b065fe" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
